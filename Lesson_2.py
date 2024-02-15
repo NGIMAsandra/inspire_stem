04bef676275845d8bdf7eb6bd334715c974bc5d9
NGIMAsandra <159754790+NGIMAsandra@users.noreply.github.com>
2024-02-15 17:49:55 +0300

    Delete Lesson_2.py

--- Lesson_2.py
+++
@@ -1,7 +0,0 @@
-print("My name is Sandra")
-print ("I am 18 years old")
-print ("I come from Kiambu county")
-print("I love watching tiktok")
-print("------------")
-print("************")
-print("############")
