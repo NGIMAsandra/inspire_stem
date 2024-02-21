e566926c47812db34cf5d9ccc17ddb73c7b5e47e
NGIMAsandra <159754790+NGIMAsandra@users.noreply.github.com>
2024-02-21 14:19:22 +0300

    Delete input.py

--- input.py
+++
@@ -1,8 +0,0 @@
-#program to get user input
-#date: 20/2/2024
-#name: sandra
-
-f_name = input("Enter your first name ")
-s_name = input("Enter your second name ")
-
-print(" my name is ", f_name + s_name)
