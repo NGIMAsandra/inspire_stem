b96079d3650427615af45657951c1704ddd82833
NGIMAsandra <159754790+NGIMAsandra@users.noreply.github.com>
2024-02-21 14:18:30 +0300

    Delete Surface area of a cylinder.py

--- Surface area of a cylinder.py
+++
@@ -1,7 +0,0 @@
-radius =float(input ("Enter the radius of the cylinder:"))
-height = float(input ("Enter the height of the cylinder:"))
-pie = float (3.142)
-Diameter = float(radius*2)
-
-surface_area = float((2 * pie *radius**2) + (Diameter * pie * height))
-print (" The surface area of the cylinder is:" ,surface_area)
