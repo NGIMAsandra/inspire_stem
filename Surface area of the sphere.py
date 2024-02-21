b52687d79f2b5734e3c8c96a0ae41140e5da6ed5
NGIMAsandra <159754790+NGIMAsandra@users.noreply.github.com>
2024-02-21 14:18:47 +0300

    Delete Surface area of the sphere.py

--- Surface area of the sphere.py
+++
@@ -1,5 +0,0 @@
-radius =float(input ("Enter the radius of the cylinder:"))
-pie = float (3.142)
-
-surface_area = float( 4 * pie * radius**2)
-print("The surface area of the sphere is:",surface_area)
