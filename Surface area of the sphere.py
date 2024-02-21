124d13988b64e6fd8e091cf2546f805d8acbe303
NGIMAsandra <159754790+NGIMAsandra@users.noreply.github.com>
2024-02-21 09:14:10 +0300

    arithmetic progression

---
+++ Surface area of the sphere.py
@@ -0,0 +1,5 @@
+radius =float(input ("Enter the radius of the cylinder:"))
+pie = float (3.142)
+
+surface_area = float( 4 * pie * radius**2)
+print("The surface area of the sphere is:",surface_area)
