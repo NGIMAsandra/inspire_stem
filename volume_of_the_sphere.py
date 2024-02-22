27c6f61dd9d7e30848998e4339f7041be919f0bb
NGIMAsandra <159754790+NGIMAsandra@users.noreply.github.com>
2024-02-22 18:27:04 +0300

    Delete volume_of_the_sphere.py

--- volume_of_the_sphere.py
+++
@@ -1,9 +0,0 @@
-#this is a  program to  get the volume of the sphere
-#date: 20/2/2024
-#name :sandra
-
-radius =float(input ("Enter the radius of the cylinder:"))
-pi = float (3.142)
-
-volume =float (4 / 3 * pi* radius**3 )
-print ("The volume of the sphere is :",volume)
